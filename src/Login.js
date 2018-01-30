55f128bb59a5dbaeda5a197832deca92c5f61be4
risman94 <kamarulrisman1994@gmail.com>
2018-01-30 21:29:24 +0800

    sip

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dataname: '',
-      datapass: ''
+      datapass: '',
+      redirect: false
     };
   }
 
   handleName(e) {
-    e.preventDefault();
     this.setState({
       dataname: e.target.value
     });
   }
 
   handlePass(e) {
-    e.preventDefault();
     this.setState({
       datapass: e.target.value
     });
   }
 
-  render() {
+  handleClick(e) {
+    e.preventDefault();
     console.log(this.state.dataname);
     console.log(this.state.datapass);
     axios
@@ -33,33 +33,43 @@ class Login extends Component {
         username: this.state.dataname,
         password: this.state.datapass
       })
-      .then(function(response) {
+      .then(response => {
         console.log(response);
+        this.setState({
+          redirect: true
+        });
       })
-      .catch(function(error) {
+      .catch(error => {
         console.log(error);
       });
+  }
+
+  render() {
     return (
       <div>
-        <p>
-          <input
-            type="text"
-            value={this.state.dataname}
-            onChange={this.handleName.bind(this)}
-          />
-        </p>
-        <p>
-          <input
-            type="password"
-            value={this.state.datapass}
-            onChange={this.handlePass.bind(this)}
-          />
-        </p>
-        <p>
-          <button>
-            <Link to="/dashboard">login</Link>
-          </button>
-        </p>
+        <form onSubmit={this.handleClick.bind(this)}>
+          <p>
+            <input
+              type="text"
+              value={this.state.dataname}
+              onChange={this.handleName.bind(this)}
+            />
+          </p>
+          <p>
+            <input
+              type="password"
+              value={this.state.datapass}
+              onChange={this.handlePass.bind(this)}
+            />
+          </p>
+          {this.state.redirect ? (
+            <Redirect to="/dashboard" />
+          ) : (
+            <p>
+              <input type="submit" value="Login" />
+            </p>
+          )}
+        </form>
       </div>
     );
   }
